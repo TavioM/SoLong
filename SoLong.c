f3d1d5760446533129c1cd36f15ffe6313a4e0bd
tavio <ocmarout@student.42.fr>
2021-09-09 18:00:06 +0200

    added a key hook function

--- SoLong.c
+++ SoLong.c
@@ -6,11 +6,17 @@
 /*   By: ocmarout <ocmarout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 18:18:47 by ocmarout          #+#    #+#             */
-/*   Updated: 2021/09/04 19:57:51 by ocmarout         ###   ########.fr       */
+/*   Updated: 2021/09/09 17:21:53 by ocmarout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int	key_hook(int keycode, int tmp)
+{
+	ft_printf("keycode = %d\n", keycode);
+	return (tmp);
+}
 
 int	main(void)
 {
@@ -19,6 +25,7 @@ int	main(void)
 
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 100, 100, "test");
+	mlx_key_hook(win, &key_hook, 0);
 	mlx_loop(mlx);
 	return (0);
 }
