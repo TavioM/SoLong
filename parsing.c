516f7f3726e666bb7018b4eb31e96e2c6c18b59c
Octave Maroutian <ocmarout@student.42.fr>
2021-10-19 17:45:14 +0200

    improved the parser

--- parsing.c
+++ parsing.c
@@ -6,13 +6,13 @@
 /*   By: ocmarout <ocmarout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 16:08:38 by ocmarout          #+#    #+#             */
-/*   Updated: 2021/10/19 15:33:43 by ocmarout         ###   ########.fr       */
+/*   Updated: 2021/10/19 17:44:45 by ocmarout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int		check_filepath(char *filepath)
+static int	check_filepath(char *filepath)
 {
 	int		i;
 	int		fd;
@@ -27,24 +27,46 @@ int		check_filepath(char *filepath)
 		return (0);
 	}
 	fd = open(filepath, O_RDONLY);
-	ft_printf("fd = %d", fd);
-	return (-1);
+	return (fd);
+}
+
+static void	add_line_to_map(char ***map, int size)
+{
+	int		i;
+	char	**tmp;
+
+	tmp = malloc(sizeof(char *) * size);
+	if (!tmp)
+		return ()
 }
 
 char	**parsing(char *filepath)
 {
-//	int		i;
+	int		i;
+	int		j;
 	int		fd;
 	char	**map;
 
 	fd = check_filepath(filepath);
 	if (fd == -1)
 		return (0);
-	map = malloc(sizeof(char *) * 3);
+	map = malloc(sizeof(char *));
 	if (!map)
 	{
+		ft_printf("Error\nmalloc failed\n");
 		close(fd);
 		return (0);
+	}
+	i = 0;
+	j = get_next_line(fd, &(map[i++]));
+	while (j == 1)
+	{
+		if (add_line_to_map(&map, i))
+		{
+			ft_printf("Error\nFailed malloc\n")
+			return (0);
+		}
+		j = get_next_line(fd, &(map[i++]));
 	}
 	return (0);
 }
