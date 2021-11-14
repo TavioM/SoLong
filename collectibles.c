95a47d7450dd8ec5def1593741509b6d08901fcd
tavio <tavio92.tm@gmail.com>
2021-11-14 15:06:34 +0100

    added a counter for collectibles

---
+++ collectibles.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   collectibles.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ocmarout <ocmarout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/12 19:56:30 by ocmarout          #+#    #+#             */
+/*   Updated: 2021/11/12 21:45:44 by ocmarout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	count_collectibles(t_mlx *mlx)
+{
+	int	i;
+	int	j;
+
+	(mlx->collectibles) = 0;
+	j = 0;
+	while (mlx->map[j])
+	{
+		i = 0;
+		while (mlx->map[j][i])
+		{
+			if (mlx->map[j][i] == 'C')
+				(mlx->collectibles)++;
+			i++;
+		}
+		j++;
+	}
+}
+
+void	collectibles(t_mlx *mlx)
+{
+	if (mlx->map[mlx->y][mlx->x] == 'C')
+	{
+		mlx->map[mlx->y][mlx->x] = '0';
+		(mlx->collectibles)--;
+	}
+	if (mlx->map[mlx->y][mlx->x] == 'E')
+		if (mlx->collectibles == 0)
+			mlx_destroy_window(mlx->mlx, mlx->win);
+}
